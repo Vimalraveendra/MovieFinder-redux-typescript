55069d5f49d951962dd2892a021fbc01bc41e483
vimal Raveendran <vimalraveendra@gmail.com>
2020-05-19 17:16:52 +0200

    Created SearchBox reducer to handle the actions

---
+++ src/Redux/SearchBox/SearchBox.reducer.js
@@ -0,0 +1,17 @@
+import SearchBoxActionTypes from "./SearchBox.types";
+
+const initialState = {
+  searchField: "",
+};
+
+export const searchChangeReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case SearchBoxActionTypes.HANDLE_SEARCH_CHANGE:
+      return {
+        ...state,
+        searchField: action.payload,
+      };
+    default:
+      return state;
+  }
+};
--- src/Redux/SearchBox/SearchBox.types.js
+++ src/Redux/SearchBox/SearchBox.types.js
@@ -1,6 +1,5 @@
 const SearchBoxActionTypes = {
   HANDLE_SEARCH_CHANGE: "HANDLE_SEARCH_CHANGE",
-  HANDLE_BUTTON_SUBMIT: "HANDLE_BUTTON_SUBMIT",
 };
 
 export default SearchBoxActionTypes;
