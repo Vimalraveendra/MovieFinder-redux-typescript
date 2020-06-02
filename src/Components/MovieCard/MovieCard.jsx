7c29ba126ba6add1b70547f3711201c7e99e1dee
vimal Raveendran <vimalraveendra@gmail.com>
2020-06-02 21:49:26 +0200

    Updated the MovieCard component image width

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -3,7 +3,7 @@ import styles from "./MovieCard.module.css";
 const MovieCard = ({ title, poster, year, error }) => {
   return title ? (
     <div className={styles.moviecard}>
-      <img src={poster} alt="movie-poster" height="350" />
+      <img src={poster} alt="movie-poster" width="250" height="350" />
       <div>
         <h2>{title}</h2>
         <p>Year:{year}</p>
