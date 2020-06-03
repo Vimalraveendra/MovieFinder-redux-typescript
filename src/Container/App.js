67de1a590c42e7b6a2e3d83d65d7c00da4e19636
vimal Raveendran <vimalraveendra@gmail.com>
2020-06-03 22:21:10 +0200

    Updated App compoenet

--- src/Container/App.js
+++ src/Container/App.js
@@ -3,35 +3,8 @@ import styles from "./App.module.css";
 import SearchBox from "../Components/SearchBox/SearchBox";
 import MovieList from "../Components/MovieList/MovieList";
 
-// import { fetchFilmData } from "../API/API";
-
 class App extends React.Component {
-  // state = {
-  //   searchField: "",
-  //   movies: [],
-  //   error: "",
-  // };
-
-  // searchChange = (e) => {
-  //   this.setState({ searchField: e.target.value });
-  // };
-
-  // clearMovies = () => {
-  //   this.setState({ movies: [], searchField: "", error: "" });
-  // };
-
-  // onHandleSubmit = async () => {
-  //   const { searchField } = this.state;
-  //   if (searchField.length > 0) {
-  //     const Search = await fetchFilmData(searchField);
-  //     this.setState({ movies: Search });
-  //   } else {
-  //     this.setState({ error: "Sorry!!!, Please enter a film name" });
-  //   }
-  // };
-
   render() {
-    // const { searchField, movies, error } = this.state;
     return (
       <div className={styles.App}>
         <h1>Movie finder</h1>
