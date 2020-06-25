fa808533a743df4655a091b33a6a65c27034d38a
vimal Raveendran <vimalraveendra@gmail.com>
2020-06-25 22:45:30 +0200

    added typescript , Updated index file and App components to typescript

--- src/Redux/rootSaga.js
+++ src/Redux/rootSaga.js
@@ -2,6 +2,8 @@ import { all, fork } from "redux-saga/effects";
 import { watchFetchFilmData } from "./FilmCard/FilmCard.sagas";
 
 export function* rootSaga() {
-  // fork performs a non-blocking operation on the function passed
+  // fork performs a non-blocking operation on the function passed.
+  // Call affect runs a function. If it returns a promise, pauses the saga
+  // until the promise is resolved.
   yield all([fork(watchFetchFilmData)]);
 }
